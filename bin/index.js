694c18960b71616390a05d745561b5887f95880d
Samuel Montambault <mont.samuel@outlook.com>
2023-01-10 11:35:02 -0500

    make ascii art for help page

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,24 @@
 #!node
 import yargs from 'yargs/yargs'
+import chalk from 'chalk'
+import boxen from 'boxen'
+
 import childProcess from 'child_process'
+import readline from 'readline'
+
 import gpt3 from './GPT3.js'
 import config from './config.js'
 import shell from './shell.js'
-import readline from 'readline'
 
-const usage = '\nUsage: clai -s <shell> -p <prompt> \n' +
- 'Translates a prompt for terminal to a specific command to execute\n'
+const logo = chalk.red(
+  '│  ___  __      __    ____ \n' +
+  '│ / __)(  )    /__\\  (_  _) \n' +
+  '│( (__  )(__  /(__)\\  _)(_  \n' +
+  '│ \\___)(____)(__)(__)(____) \n│\n│')
+
+const usage = logo + chalk.red('Usage: clai -s <shell> -p <prompt> \n') +
+  boxen(chalk.green('Translates a prompt to a specific shell command to execute.'),
+    { padding: 1, borderColor: 'green', dimBorder: true })
 
 const options = yargs(process.argv.slice(2))
   .usage(usage)
@@ -15,8 +26,8 @@ const options = yargs(process.argv.slice(2))
   .describe('c', 'Configure your API key and organization ID for OpenAI')
   .alias('s', 'shell')
   .describe('s', 'Choose a default shell to execute the command against (it needs to exist in the system)')
-  .choices('s', ['bash', 'powershell', 'cmd (windows)', 'zsh'])
-  .option('p', { alias: 'prompt', describe: 'Prompt to translate the command for', type: 'string' })
+  .choices('s', ['bash', 'powershell', 'cmd', 'zsh'])
+  .option('p', { alias: 'prompt', describe: 'Prompt to translate the command for', type: 'string', demandOption: true })
   .help(true)
   .argv
 

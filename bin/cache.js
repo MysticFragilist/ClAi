374874ccf54e1b39d3ccfaafbffa2596bc0fb74d
Samuel Montambault <mont.samuel@outlook.com>
2023-01-14 23:06:56 -0500

    first draft cache

---
+++ bin/cache.js
@@ -0,0 +1,69 @@
+
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+
+const version = '1.0'
+
+const pathConfigDir = path.join(os.homedir(), '.clairc')
+const pathCache = path.join(pathConfigDir, 'cache.json')
+
+function getCaches () {
+  if (!fs.existsSync(pathCache)) {
+    fs.mkdirSync(pathConfigDir, { recursive: true })
+    fs.closeSync(fs.openSync(pathCache, 'w'))
+    fs.writeFileSync(pathCache, JSON.stringify({ version, caches: [] }))
+  }
+
+  try {
+    return JSON.parse(fs.readFileSync(pathCache)).caches
+  } catch (e) {
+    console.error('Error reading caches file. Try removing the folder HOME/.clairc/')
+    process.exit(1)
+  }
+}
+
+function addCacheEntry (prompt, command) {
+  try {
+    const cacheFile = JSON.parse(fs.readFileSync(pathCache))
+    // clear caches on version mismatch
+    if (cacheFile.version !== version) {
+      cacheFile.caches = []
+    }
+
+    // keep only the last 100 entries
+    if (cacheFile.caches.length > 100) {
+      cacheFile.caches.shift()
+    }
+
+    // add new entry if not already present
+    if (!cacheFile.caches.find(c => c.prompt === prompt && c.command === command)) {
+      cacheFile.caches.push({ prompt, command, timestamp: new Date().getTime() })
+    }
+
+    fs.writeFileSync(pathCache, JSON.stringify(cacheFile))
+  } catch (e) {
+    console.error('Error reading caches file. Try removing the folder HOME/.clairc/')
+    process.exit(1)
+  }
+}
+
+function searchCache (prompt) {
+  try {
+    const cacheFile = JSON.parse(fs.readFileSync(pathCache))
+    const cache = cacheFile.caches.find(c => c.prompt === prompt)
+    if (cache) {
+      return cache.command
+    }
+    return null
+  } catch (e) {
+    console.error('Error reading caches file. Try removing the folder HOME/.clairc/')
+    process.exit(1)
+  }
+}
+
+export default {
+  getCaches,
+  addCacheEntry,
+  searchCache
+}
--- bin/config.js
+++ bin/config.js
@@ -3,20 +3,22 @@ import os from 'os'
 import process from 'process'
 import path from 'path'
 
-const pathConfig = path.join(os.homedir(), '.clairc')
+const pathConfigDir = path.join(os.homedir(), '.clairc')
+const pathConfig = path.join(pathConfigDir, 'clai.conf')
 
 function getConfig () {
   if (!fs.existsSync(pathConfig)) {
     // Init empty
+    fs.mkdirSync(pathConfigDir, { recursive: true })
     fs.closeSync(fs.openSync(pathConfig, 'w'))
     fs.writeFileSync(pathConfig, JSON.stringify({ apiKey: '', orgId: '' }))
-    console.error('Error reading config file. Try doing `clairc config` to set your API key and organization ID.')
+    console.error('Error reading config file. Try doing `clai --config` to set your API key and organization ID.')
     process.exit(1)
   }
   try {
     return JSON.parse(fs.readFileSync(pathConfig))
   } catch (e) {
-    console.error('Error reading config file. Try doing `clairc config` to set your API key and organization ID.')
+    console.error('Error reading config file. Try doing `clai --config` to set your API key and organization ID.')
     process.exit(1)
   }
 }
